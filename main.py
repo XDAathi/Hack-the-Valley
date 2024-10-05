19ac641c8303d65aa1dbdfdce482e26e2b87dd83
Aathithya Ananth <aathithyaananth@gmail.com>
2024-10-05 15:54:40 -0400

    Reviews for Location using Location ID found using Places API, returns top 5 reveiws. stored in reveiws as a dictionary

--- main.py
+++ main.py
@@ -5,3 +5,19 @@ import config
 API_KEY = config.api_key
 
 gmaps = googlemaps.Client(API_KEY)
+placeName = "CN Tower"
+places_result = gmaps.places(placeName)
+placeID = places_result['results'][0]['place_id']
+print(placeID)
+
+place = gmaps.place(place_id = placeID)
+reviews = []
+for i in range(len(place['result']['reviews'])):
+    text = place['result']['reviews'][i]['text']
+    rating = place['result']['reviews'][i]['rating']
+    reviews.append({'rating':rating,'text':text})
+
+df = pd.DataFrame(reviews)
+print(df)
+
+print(reviews[1])
